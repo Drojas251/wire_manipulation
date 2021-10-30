8110875be3377b4b306b4718a4d912db3c4f0cef
Diego Rojas <52464724+Drojas251@users.noreply.github.com>
2021-10-30 01:02:29 -0700

    added new classes

--- testing/wire_sim.py
+++ testing/wire_sim.py
@@ -14,23 +14,109 @@ import math
 # Pass 8x3x20 to Optimizer function to select an solution and return the Pick and Pull pair that goes with that solution 
 # Return Pick and Pull pair 
 
+
+
+class DualRobotConfig:
+    def __init__(self,left_robot_location, right_robot_location,robot_reach,distance_apart):
+        self.r1 = left_robot_location # robot 1 is the robot on the left ( if you are standing behind them) 3x1 vector
+        self.r2 = right_robot_location # robot 2 is the robot on the right ( if you are standing behind them) 3x1 vector
+        self.robot_reach = robot_reach # the maximum safe reach of the robot- 1x1 scalar
+        self.distance_apart = distance_apart # lateral distance between the two robots ( assume then are next to eachother)- 1x1 scalar
+
+    def workspace_volume(self):
+        workspace_volume = np.zeros((3,8))
+
+        scale_on_x = 0.75
+        scale_on_z = 0.85
+        x = scale_on_x*self.robot_reach
+        y = self.robot_reach - self.distance_apart
+        z = scale_on_z*self.robot_reach
+
+        workspace_volume[:,[0]] = np.array([self.r2[0] + x, self.r2[1] + y, self.r2[2] + z]) # top right forward 
+        workspace_volume[:,[1]] = np.array([self.r1[0] + x, self.r1[1] - y, self.r1[2] + z]) # top left forward 
+        workspace_volume[:,[2]] = np.array([self.r2[0] + x, self.r2[1] + y, self.r2[2]]) # bottom right forward 
+        workspace_volume[:,[3]] = np.array([self.r1[0] + x, self.r1[1] - y, self.r1[2]]) # bottom left forward 
+
+        workspace_volume[:,[4]] = np.array([self.r2[0] - x, self.r2[1] + y, self.r2[2] + z]) # top right back 
+        workspace_volume[:,[5]] = np.array([self.r1[0] - x, self.r1[1] - y, self.r1[2] + z]) # top left back 
+        workspace_volume[:,[6]] = np.array([self.r2[0] - x, self.r2[1] + y, self.r2[2]]) # bottom right back 
+        workspace_volume[:,[7]] = np.array([self.r1[0] - x, self.r1[1] - y, self.r1[2]]) # bottom left back
+
+        return workspace_volume 
+
+    def mid_point_of_reference(self):
+        mid_point = np.array([self.r1[0], (self.r1[1] + self.r2[1])/2, 0.6*self.robot_reach])
+        return mid_point
+
+
+class GraspObject:
+    def __init__(self,gop,god,rx,ry,rz): 
+        self.gop = gop # grasp object position
+        self.god = god # grasp object dimension
+        self.rx = rx # rotations
+        self.ry = ry
+        self.rz = rz
+        self.normal_axis = np.array([[1],[0],[0]]) # normal axis of the object is always in the +x axis of the object frame 
+
+    def rotation_matix(self):
+      rotation_x = np.array([[1,0,0],[0, math.cos(self.rx), -1*math.sin(self.rx)],[0,math.sin(self.rx),math.cos(self.rx)]])
+      rotation_y = np.array([[math.cos(self.ry), 0 , math.sin(self.ry)], [0 , 1, 0], [-1*math.sin(self.ry), 0 , math.cos(self.ry)]])
+      rotation_z = np.array([[math.cos(self.rz), -1*math.sin(self.rz), 0], [math.sin(self.rz), math.cos(self.rz), 0], [0, 0, 1]])
+      ROT = rotation_x@rotation_y@rotation_z
+      return ROT
+
+    def grasp_volume_in_local_frame(self):
+        # forward is in the +x axis 
+        dx = self.god[0]/2
+        dy = self.god[1]/2
+        dz = self.god[2]/2
+
+        grasp_volume_in_local_frame = np.zeros((3,8))
+
+        grasp_volume_in_local_frame[:,[0]] = np.array([dx, dy, dz]) # top right forward 
+        grasp_volume_in_local_frame[:,[1]] = np.array([dx, -dy, dz]) # top left forward 
+        grasp_volume_in_local_frame[:,[2]] = np.array([dx, dy, -dz]) # bottom right forward 
+        grasp_volume_in_local_frame[:,[3]] = np.array([dx, -dy, -dz]) # bottom left forward 
+
+        grasp_volume_in_local_frame[:,[4]] = np.array([-dx, dy, dz]) # top right back 
+        grasp_volume_in_local_frame[:,[5]] = np.array([-dx, -dy, dz]) # top left back 
+        grasp_volume_in_local_frame[:,[6]] = np.array([-dx, dy, -dz]) # bottom right back 
+        grasp_volume_in_local_frame[:,[7]] = np.array([-dx, -dy, -dz]) # bottom left back 
+
+        return grasp_volume_in_local_frame
+    
+    def grasp_volume_in_global_frame(self):
+        # grasp volume in the global frame
+        grasp_volume_in_local_frame = self.grasp_volume_in_local_frame()
+        ROT = self.rotation_matix()
+        grasp_volume_in_global_frame = np.zeros((3,8))
+
+        for i in range(8):
+            grasp_volume_in_global_frame[:,[i]] = self.gop + ROT@grasp_volume_in_local_frame[:,[i]]
+
+        return grasp_volume_in_global_frame
+
+class WireModel:
+    def __init__(self,L,M,Ks,Kb,Kd): 
+        self.L = L #length of wire
+        self.M = M # mass of wire
+        self.Ks = Ks # Stifness of structural spring
+        self.Kb = Kb # stiffness of bending spring
+        self.Kd = Kd # damping
+
+        
+
+    
+
 class WireSim:
-  def __init__(self,N,L,M,gop,god,rx,ry,rz):
+  def __init__(self,N,wire_model, grasp_object,robot):
     self.N = N # number of nodes that represents the wire
-    self.Ks = 13.1579 # Stifness of structural spring
-    self.Kb = 6.13 # stiffness of bending spring
-    self.Kd = 5.26 # damping 
-    self.L = L #length of wire
-    self.M = M # mass of wire
-    self.m = self.M/self.N # individual node mass
-    self.l = self.L/(self.N - 1) # resting length between nodes
-    self.gop = gop # grasp object position
-    self.god = god # grasp object dimension
-    self.rx = rx # rotations
-    self.ry = ry
-    self.rz = rz
+    self.wire_model = wire_model
+    self.m = self.wire_model.M/self.N # individual node mass
+    self.l = self.wire_model.L/(self.N - 1) # resting length between nodes
     self.actions = 8
-    self.normal_axis = np.array([[1],[0],[0]]) # normal axis of the object is always in the +x axis of the object frame 
+    self.grasp_object = grasp_object
+    self.robot = robot
     
 
   def __force_for_struct_springs(self,wire,V):
@@ -51,24 +137,24 @@ class WireSim:
 
             # Calculate spring force on node i from neighboring nodes
             if i == 1:
-                F1 = 2*self.Ks*((self.l/l_prime_2) - 1)*(wire[:,[i]] - wire[:,[i-1]])
-                F2 = -self.Ks*((self.l/l_prime_1) - 1)*(wire[:,[i+1]] - wire[:,[i]])
+                F1 = 2*self.wire_model.Ks*((self.l/l_prime_2) - 1)*(wire[:,[i]] - wire[:,[i-1]])
+                F2 = -self.wire_model.Ks*((self.l/l_prime_1) - 1)*(wire[:,[i+1]] - wire[:,[i]])
 
             elif i == (self.N - 2):
-                F1 = self.Ks*((self.l/l_prime_2) - 1)*(wire[:,[i]] - wire[:,[i-1]])
-                F2 = -2*self.Ks*((self.l/l_prime_1) - 1)*(wire[:,[i+1]] - wire[:,[i]])
+                F1 = self.wire_model.Ks*((self.l/l_prime_2) - 1)*(wire[:,[i]] - wire[:,[i-1]])
+                F2 = -2*self.wire_model.Ks*((self.l/l_prime_1) - 1)*(wire[:,[i+1]] - wire[:,[i]])
 
             else:
-                F1 = self.Ks*((self.l/l_prime_2) - 1)*(wire[:,[i]] - wire[:,[i-1]])
-                F2 = -self.Ks*((self.l/l_prime_1) - 1)*(wire[:,[i+1]] - wire[:,[i]])
+                F1 = self.wire_model.Ks*((self.l/l_prime_2) - 1)*(wire[:,[i]] - wire[:,[i-1]])
+                F2 = -self.wire_model.Ks*((self.l/l_prime_1) - 1)*(wire[:,[i+1]] - wire[:,[i]])
 
 
             # INCLUDE DAMPING FORCE
             x1 = wire[:,[i]] - wire[:,[i-1]]
-            F1_d = -self.Kd*np.matmul(x1*np.transpose(x1), V[:,[i]] - V[:,[i-1]])/(la.norm(x1)**2)
+            F1_d = -self.wire_model.Kd*np.matmul(x1*np.transpose(x1), V[:,[i]] - V[:,[i-1]])/(la.norm(x1)**2)
 
             x2 = wire[:,[i+1]] - wire[:,[i]]
-            F2_d = self.Kd*np.matmul(x2*np.transpose(x2), V[:,[i+1]] - V[:,[i]])/(la.norm(x2)**2)
+            F2_d = self.wire_model.Kd*np.matmul(x2*np.transpose(x2), V[:,[i+1]] - V[:,[i]])/(la.norm(x2)**2)
 
 
 
@@ -86,18 +172,18 @@ class WireSim:
           # Case 1: forces on 2nd and 3rd nodes 
           if i == 1 or i == 2:
               l_prime = la.norm(wire[:,[i+2]] - wire[:,[i]])
-              F_b[:,[i]] = -self.Kb*((self.l/l_prime) - 1)*wire[:,[i+2]] - wire[:,[i]] 
+              F_b[:,[i]] = -self.wire_model.Kb*((self.l/l_prime) - 1)*wire[:,[i+2]] - wire[:,[i]] 
           # Case 2: forces on 18th and 19th node
           elif i == (self.N - 2) or i == (self.N -3):
               l_prime = la.norm(wire[:,[i]] - wire[:,[i-2]])
-              F_b[:,[i]] = self.Kb*((self.l/l_prime) - 1)*wire[:,[i]] - wire[:,[i-2]]
+              F_b[:,[i]] = self.wire_model.Kb*((self.l/l_prime) - 1)*wire[:,[i]] - wire[:,[i-2]]
           # All other nodes    
           else:
               l_prime_1 = la.norm(wire[:,[i+2]] - wire[:,[i]]) # new distance from node i to node (i+2)
               l_prime_2 = la.norm(wire[:,[i]] - wire[:,[i-2]]) # new distance from node i to node (i-2)
 
-              F1 = -self.Kb*((self.l/l_prime_1) - 1)*wire[:,[i+2]] - wire[:,[i]]
-              F2 = self.Kb*((self.l/l_prime_2) - 1)*wire[:,[i]] - wire[:,[i-2]]
+              F1 = -self.wire_model.Kb*((self.l/l_prime_1) - 1)*wire[:,[i+2]] - wire[:,[i]]
+              F2 = self.wire_model.Kb*((self.l/l_prime_2) - 1)*wire[:,[i]] - wire[:,[i-2]]
 
               F_b[:,[i]] = F1 + F2
 
@@ -281,20 +367,33 @@ class WireSim:
       rotation_z = np.array([[math.cos(rz), -1*math.sin(rz), 0], [math.sin(rz), math.cos(rz), 0], [0, 0, 1]])
       ROT = rotation_x@rotation_y@rotation_z
       return ROT
+  def homo_trans_matrix(self,translation,ROT):
+      HTM = np.array([[ROT[0][0], ROT[0][1], ROT[0][2], translation[0][0]],
+                    [ROT[1][0],ROT[1][1],ROT[1][2],translation[1][0]],
+                    [ROT[2][0],ROT[2][1],ROT[2][2],translation[2][0]],
+                    [0 , 0, 0, 1]])
+      return HTM
 
-  def projections(self,wire_set):
+  def vector_to_homo(self,vector):
+      homo_vector = np.array([[vector[0][0]], [vector[1][0]], [vector[2][0]], [1] ])
+      return homo_vector
 
-      projections = np.zeros((self.actions,3,self.N))
-      ROT = self.rotation_matix(self.rx, self.ry, self.rz)
-      grasp_axis = ROT@self.normal_axis
+  def homo_to_vector(self,homo):
+      vector = np.array([[homo[0][0]], [homo[1][0]], [homo[2][0]] ])
+      return vector
 
-      ## getting an error with matrix multiplication
-
-      for i in range(8):
-          for j in range(self.N):
-              projections[[i],:,[j]] = wire_set[[i],:,[j]] - self.dot_product((np.transpose(wire_set[[1],:,[2]]) - self.gop),grasp_axis)*grasp_axis
+  #def projections(self,wire_set):
+  #    # Exlude for now
+  #    projections = np.zeros((self.actions,3,self.N))
+  #    ROT = self.rotation_matix(self.grasp_object.rx, self.grasp_object.ry, self.grasp_object.rz)
+  #    grasp_axis = ROT@self.grasp_object.normal_axis
+  #
+  #    ## getting an error with matrix multiplication
+  #    #for i in range(8):
+  #    #    for j in range(self.N):
+  #    #        projections[[i],:,[j]] = wire_set[[i],:,[j]] - self.dot_product((np.transpose(wire_set[[1],:,[2]]) - self.gop),grasp_axis)*grasp_axis
     
-      print(projections)
+     
 
 
 
@@ -304,20 +403,40 @@ class WireSim:
 
           
 
+L = 1
+M = 0.028
+Ks = 13.1579
+Kb = 6.13
+Kd = 5.26
+wire_model = WireModel(L,M,Ks,Kb,Kd)
 
 
 # grasp Object Info -> use a custom ros message to pack this information
-grasp_object_position = np.array([[1.1],[0.75],[0.45]]) # position of grasp object
-grasp_object_dimension = np.array([[0.075],[0.05],[0.05]]) # dimensions of grasp object -> model the grasp object as a box
+gop = np.array([[1],[0.25],[0.6]]) # position of grasp object
+god = np.array([[0.075],[0.05],[0.05]]) # dimensions of grasp object -> model the grasp object as a box
 # orientation of grasp object wrt to world in order (xyz)
 rx = 0 # rot about x
 ry = 0 # rot about y
-rz = -2*3.14/3 # rot about z
+rz = 1.5707 # rot about z
+
+grasp_object = GraspObject(gop,god,rx,ry,rz)
+ROT = grasp_object.rotation_matix()
+
+
+# dual robot configuration
+left_robot_location = np.array([[0],[0.1],[0]])
+right_robot_location = np.array([[0],[-0.1],[0]])
+robot_reach = 0.5
+distance_apart = 0.3
+R1 = DualRobotConfig(left_robot_location, right_robot_location,robot_reach,distance_apart)
+
+
+# wire sim set up
 N = 20
 L = 1
 M = 0.028
+wire_sim_ = WireSim(N,wire_model,grasp_object, R1 )
 
-wire_sim_ = WireSim(N,L,M,grasp_object_position, grasp_object_dimension, rx, ry, rz )
 
 
 wire = np.zeros((3,N))
@@ -326,6 +445,9 @@ wire[1] = np.linspace(0,1,N)
 wire[2] = 2*np.ones((1,N))
 
 #normal_vectors = p1.get_normal_vectors(test,10)
-sim_result = wire_sim_.simulate(wire)
+#sim_result = wire_sim_.simulate(wire)
 
-
+# TO DO
+# port the following functions from MATLAB in the following order
+#
+# find_optimal_vector.m
