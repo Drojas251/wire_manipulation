77d839a64c7e9f18ddd1c676c72dedfefc976e11
Drojas251 <52464724+Drojas251@users.noreply.github.com>
2021-11-11 23:34:13 -0800

    sorting nodes and removing outlier

--- vision/src/point_cloud_processing.py
+++ vision/src/point_cloud_processing.py
@@ -15,10 +15,40 @@ from geometry_msgs.msg import Point
 from geometry_msgs.msg import Pose
 from sklearn.cluster import KMeans
 
+
+def sort_points(points,end_point):
+    elements = []
+    elements.append(end_point)
+
+    sorted_points = np.zeros((len(points), 3))
+    sorted_points[0,:] = points[int(end_point),:]
+
+    for i in range(len(points) - 1):
+        min_dist = 5
+        for j in range(len(points)):
+            compare = True
+            for ele in elements:
+                if j == ele:
+                    compare = False
+            if compare == True:
+                dist = ((points[j,0] - sorted_points[i,0])**2 + (points[j,1] - sorted_points[i,1])**2 + (points[j,2] - sorted_points[i,2])**2 )**0.5
+                if dist < min_dist:
+                    min_dist = dist
+                    element = j
+        sorted_points[i+1,:] = points[element,:]
+        elements.append(element)
+    return sorted_points
+
+
+
+
 def callback(data):
     pc = ros_numpy.numpify(data)
     points=np.zeros((len(pc)*len(pc[0]),3))
     count = 0
+    N = 20
+
+    translation = np.array([0, 0, 0])
 
     # format the processed pointcloud into a N x 3 numpy array
     for x in range(len(pc)):
@@ -26,25 +56,26 @@ def callback(data):
             points2 = pc[x,y]
             PP = [points2[0],points2[1],points2[2]]
             if not math.isnan(points2[1]):
-                points[count] = [points2[0],points2[1],points2[2]]   
+                # transform the points into the world frame
+                points[count] = [points2[0],points2[1],points2[2]] + translation  
                 count = count + 1    
 
     # After "count" all elements should be zero. Extract only non-zero elements
     points_ = points[0:(count-1), :]
 
     # cluster the point cloud data into N = 20 nodes
-    kmeans = KMeans(n_clusters=20)
+    kmeans = KMeans(n_clusters=N)
     kmeans.fit(points_)
     #print(kmeans.labels_)
     C = kmeans.cluster_centers_
     
     # Find outliers 
     outliers = []
-    threshold = 0.1 # a point whos closest neighbor is greater than 0.1m away is considered an outlier 
+    threshold = 0.12 # a point whos closest neighbor is greater than 0.1m away is considered an outlier 
 
-    for k in range(20):
+    for k in range(N):
         min_dist = 5
-        for j in range(20):
+        for j in range(N):
             if k != j:
                 dist = ((C[k,0] - C[j,0])**2 + (C[k,1] - C[j,1])**2 + (C[k,2] - C[j,2])**2 )**0.5
                 if dist < min_dist:
@@ -54,26 +85,67 @@ def callback(data):
         if min_dist > threshold: 
             outliers.append(k)
         
-    """ # define pose array
-    poses_ = PoseArray()
-    poses_.header.frame_id = 'camera_link'
+    # Get rid of outliers 
+    # reduce your array
+    size_of_outliers = len(outliers)
+    new_points = np.zeros((N-size_of_outliers,3))
 
-    for i in range(20):
-        pose = Pose()
-        pose.position.x = C[i,0]
-        pose.position.y = C[i,1]
-        pose.position.z = C[i,2]
-        pose.orientation.x = 0
-        pose.orientation.y = 0
-        pose.orientation.z = 0
-        pose.orientation.w = 1
-        
-        poses_.poses.append(pose)
-    """
+    for i in range(N):
+        remove = False
+        for k in range(size_of_outliers):
+            if i == size_of_outliers[k]:
+                remove = True
+        if(remove != True):
+            new_points[[i],:] = C[[i],:]
+
+    # check the extreme points ( maz min x,y,z)
+    # find the element with the most extreme values
+    # ex. element 6 might hold the max x and min z. Likley an end point
+    # use this point as a starting point
+
+    min_y = max_y = new_points[0,1]
+    min_z = max_z = new_points[0,2]
+    extrema_elements = np.zeros(4) # [min_y, max_y, min_z, max_z]
+
+    for j in range(len(new_points)):
+        if new_points[j,1] <= min_y:
+            min_y = new_points[j,1]
+            extrema_elements[0] = int(j)
+        if new_points[j,1] >= max_y:
+            max_y = new_points[j,1]
+            extrema_elements[1] = int(j)
+        if new_points[j,2] <= min_z:
+            min_z = new_points[j,2]
+            extrema_elements[2] = int(j)
+        if new_points[j,1] >= max_z:
+            max_z = new_points[j,2]
+            extrema_elements[3] = int(j)
+
+
+    distance_between_y_extrema = np.abs(max_y - min_y)
+
+    if distance_between_y_extrema > 0.1:
+        print("Y end point")
+        print("end point is either" + str(extrema_elements[0]) + "or" + str(extrema_elements[1]))
+        end_point = int(extrema_elements[0])
+    else:
+        print("Z end point")
+        print("end point is either" + str(extrema_elements[2]) + "or" + str(extrema_elements[3]))
+        end_point = int(extrema_elements[2])
+
+    # temporation function. just looking for end point in original array C. Remove late 
+    for i in range(N):
+        if C[i,0] == new_points[end_point,0] and C[i,1] == new_points[end_point,1] and C[i,2] == new_points[end_point,2]:
+            end_point_C = i
+            break
+
+    sorted_points = sort_points(new_points,end_point)
+    
+
     count = 0
     markers = MarkerArray()
 
-    for j in range(20):
+    """for j in range(20):
 
         marker_object = Marker()
         marker_object.header.frame_id = 'camera_color_optical_frame'
@@ -95,23 +167,26 @@ def callback(data):
         marker_object.pose.orientation.y = 0
         marker_object.pose.orientation.z = 0
 
-        marker_object.scale.x = 0.085
-        marker_object.scale.y = 0.085
-        marker_object.scale.z = 0.085
+        marker_object.scale.x = 0.025
+        marker_object.scale.y = 0.025
+        marker_object.scale.z = 0.025
 
         # color the outlier in blue
         # TO DO: Remove outlier from data set
-        for out in outliers:
-            if marker_object.id == out:
-                marker_object.color.r = 0.0
-                marker_object.color.g = 0.0
-                marker_object.color.b = 1.0
-                break
-
-            else:
-                marker_object.color.r = 1.0
-                marker_object.color.g = 0.0
-                marker_object.color.b = 0.0
+        #for out in outliers:
+            #if marker_object.id == out:
+            #    marker_object.color.r = 0.0
+            #    marker_object.color.g = 0.0
+            #    marker_object.color.b = 1.0
+                
+        if marker_object.id == end_point_C:
+            marker_object.color.r = 0.0
+            marker_object.color.g = 1.0
+            marker_object.color.b = 0.0
+        else:
+            marker_object.color.r = 1.0
+            marker_object.color.g = 0.0
+            marker_object.color.b = 0.0
 
         marker_object.color.a = 1.0
         marker_object.lifetime = rospy.Duration(0)
@@ -119,6 +194,47 @@ def callback(data):
         markers.markers.append(marker_object)
 
         count = count + 1
+    """
+    for j in range(len(sorted_points)):
+
+        marker_object = Marker()
+        marker_object.header.frame_id = 'camera_color_optical_frame'
+        marker_object.header.stamp = rospy.get_rostime()
+        marker_object.ns = 'point'
+        marker_object.id = count + j
+        marker_object.type = Marker.SPHERE
+        marker_object.action = Marker.ADD
+
+        my_point = Point()
+        my_point.x = sorted_points[j,0]
+        my_point.y = sorted_points[j,1]
+        my_point.z = sorted_points[j,2]
+        
+        marker_object.pose.position = my_point
+
+        marker_object.pose.orientation.w = 1
+        marker_object.pose.orientation.x = 0
+        marker_object.pose.orientation.y = 0
+        marker_object.pose.orientation.z = 0
+
+        marker_object.scale.x = 0.025
+        marker_object.scale.y = 0.025
+        marker_object.scale.z = 0.025
+                
+        if marker_object.id%2 == 0:
+            marker_object.color.r = 0.0
+            marker_object.color.g = 0.0
+            marker_object.color.b = 1.0
+        else:
+            marker_object.color.r = 0.5
+            marker_object.color.g = 0.0
+            marker_object.color.b = 0.5
+
+        marker_object.color.a = 1.0
+        marker_object.lifetime = rospy.Duration(0)
+
+        markers.markers.append(marker_object)
+        
 
     marker_.publish(markers)    
 
