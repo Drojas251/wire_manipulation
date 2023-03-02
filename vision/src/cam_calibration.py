bed1f29ebee250968e9081530eda7b5421b19953
Lawrence Hwang <lawrenceh326@gmail.com>
2023-03-01 17:37:56 -0800

    Complete script + helper functions TODO Test w board

--- vision/src/cam_calibration.py
+++ vision/src/cam_calibration.py
@@ -16,10 +16,10 @@ class CameraCalibration(object):
 
     def calibrate(self, directory_path, img_file_prefix, img_format, square_size, height, width):
         # Define board matrix mapping calibration sheet
-        object_points = np.zeros((height * width, 3), np.float32)
-        object_points[:, :2] = np.mgrid[0:width, 0:height].T.reshape(-1, 2)
+        object_point = np.zeros((height * width, 3), np.float32)
+        object_point[:, :2] = np.mgrid[0:width, 0:height].T.reshape(-1, 2)
         # Represent square_size in meters, width/height are number of squares each way
-        object_points *= square_size
+        object_point *= square_size
 
         # Define images and loop to find corners on board
         images = glob.glob(directory_path + '/' + img_file_prefix + "*."  + img_format)
@@ -30,9 +30,40 @@ class CameraCalibration(object):
             ret, corners = cv2.findChessboardCorners(gray, (width, height), None)
             # If corners found, add and store to our result
             if ret:
-                
+                self.points_3d.append(object_point)
+                processed_corners = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), self.criteria) # might need to adjust winSize
+                self.points_2d.append(processed_corners)
 
+                # Draw and display the found corners for verification
+                preview = cv2.drawChessboardCorners(image, (width, height), processed_corners, ret)
 
+        # Finally, calibrate and return matrices of image
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(self.points_3d, self.points_2d, gray.shape[::-1], None, None)
+        return [ret, mtx, dist, rvecs, tvecs]
+    
+    ## Helpful functions for image calibration
+    def save_coefficients(mtx, dist, path):
+        """
+        Save the camera matrix and the distortion coefficients to given path/file.
+        """
+        cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_WRITE)
+        cv_file.write("K", mtx)
+        cv_file.write("D", dist)
+        cv_file.release()
+    
+    def load_coefficients(path):
+        """
+        Loads camera matrix and distortion coefficients.
+        """
+        # FILE_STORAGE_READ
+        cv_file = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
+
+        camera_matrix = cv_file.getNode("K").mat()
+        dist_matrix = cv_file.getNode("D").mat()
+
+        cv_file.release()
+        return [camera_matrix, dist_matrix]
+    
 
 def main():
     # rospy.init_node("camera_calibration")
