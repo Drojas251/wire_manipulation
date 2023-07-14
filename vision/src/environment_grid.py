1b76a9cdc66832c35337de804f55846b55c405a9
Lawrence Hwang <lawrenceh326@gmail.com>
2023-07-14 15:54:21 -0700

    Calculate and draw grid, grid vertices, centerpts

--- vision/src/environment_grid.py
+++ vision/src/environment_grid.py
@@ -24,6 +24,12 @@ class EnvironmentGrid:
         self.bridge_object = CvBridge()
         self.seg_depth_img = Image()
 
+        # Grid variables
+        self.grid = None
+        self.grid_x = None
+        self.grid_y = None
+        self.midpoints = None
+
     def grid_callback(self, data):
         try:
             frame = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
@@ -31,23 +37,60 @@ class EnvironmentGrid:
             print(e)
         rospy.sleep(0.01)
 
-        frame = self.draw_grid(frame, (12, 20))
+        if not self.grid:
+            self.grid,self.midpoints = self.build_grid(frame, (12,20))
+
+        frame = self.draw_grid(frame)
+        frame = self.draw_pts(frame)
         # Display the resulting frame
         cv2.imshow('frame', frame) 
         cv2.waitKey(1)
 
-    def draw_grid(self, img, grid_shape, color=(0, 0, 255), thickness=1):
+    def build_grid(self, img, grid_shape):
         h, w, _ = img.shape
         rows, cols = grid_shape
         dy, dx = h / rows, w / cols
 
+        # Trim grid to working environment
+        dropped_col = 4
+        dropped_col_end = 2
+        dropped_row = 2
+        self.grid_x = np.linspace(start=dx, stop=w-dx, num=cols-1)[dropped_col:-dropped_col_end]
+        self.grid_y = np.linspace(start=dy, stop=h-dy, num=rows-1)[dropped_row:]
+
+        grid = []
+        midpoints = []
+        # Calculate grid x,y intersections
+        for x in self.grid_x:
+            for y in self.grid_y:
+                grid.append((x,y))
+        # Calculate grid center points
+        for x in self.grid_x[:-1]:
+            for y in self.grid_y[:-1]:
+                midpoints.append((x+dx/2, y+dy/2))
+
+        return grid, midpoints
+    
+    def draw_pts(self, img):
+        # Draw grid points
+        for coord in self.grid:
+            img = cv2.circle(img, (int(coord[0]), int(coord[1])), radius=4, color=(0, 255, 0), thickness=-1)
+        # Draw midpoints
+        for coord in self.midpoints:
+            img = cv2.circle(img, (int(coord[0]), int(coord[1])), radius=4, color=(0, 255, 0), thickness=-1)
+
+        
+        return img
+
+    def draw_grid(self, img, color=(0, 0, 255), thickness=1):
+        h, w, _ = img.shape
         # draw vertical lines
-        for x in np.linspace(start=dx, stop=w-dx, num=cols-1):
+        for x in self.grid_x:
             x = int(round(x))
             cv2.line(img, (x, 0), (x, h), color=color, thickness=thickness)
 
         # draw horizontal lines
-        for y in np.linspace(start=dy, stop=h-dy, num=rows-1):
+        for y in self.grid_y:
             y = int(round(y))
             cv2.line(img, (0, y), (w, y), color=color, thickness=thickness)
 
